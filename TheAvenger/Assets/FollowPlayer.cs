1a8ce55f674d3770377928c9efc3f4b2f2f6849b
ninoski404 <ninoski232@gmail.com>
2018-05-27 12:37:57 +0200

    Camera and player movement

---
+++ TheAvenger/Assets/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform Player;       //reference to player
+    public Vector3 offset;         // variable to offset
+
+    void Update()
+    {
+        transform.position = Player.position + offset;
+        transform.rotation = Quaternion.Euler(65, 0, 0);
+    }
+
+   
+   
+}
