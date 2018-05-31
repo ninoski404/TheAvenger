203355c33e38ca18bf11a67a123a72de464e6de6
ninoski404 <ninoski232@gmail.com>
2018-05-31 17:23:00 +0200

    added houses assets

---
+++ TheAvenger/Assets/Scripts/CartMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartMovement : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+    public float RotateSpeed = 30f;
+   
+    // Use this for initialization
+    void Start () {
+        
+            rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("i"))
+        rb.AddForce(transform.forward * thrust);
+      
+        if (Input.GetKey("j"))
+            transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey("l"))
+            transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+    }
+    
+   
+}
+
+    
+
+
